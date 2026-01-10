198d80c9242d7200d800c4e30d744077085c4ae2
2riniar <watano1168@gmail.com>
2026-01-11 01:37:14 +0900

    [PR Review] (5) _enemiesフィールドがnullの状態でアクセスするとNullReferenceExceptionが発生

--- SampleUnityProject/Assets/Scripts/GameManager.cs
+++ SampleUnityProject/Assets/Scripts/GameManager.cs
@@ -21,6 +21,7 @@ namespace SampleGame
 
         private void ProcessFirstEnemy()
         {
+            if (_enemies == null || _enemies.Count == 0) return;
             var first = _enemies[0];
             first.TakeDamage(10);
         }
